aba999c4246928f6bf9d5e5ccad1b8e46d803619
mo1699 <mo1699@gmail.com>
2023-10-17 00:07:37 +0200

    auto push

--- test/moo.c
+++ test/moo.c
@@ -5,6 +5,8 @@
 
 int main()
 {
+	
 	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
 	return 0;
 }
