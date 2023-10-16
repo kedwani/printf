bd2417e74987fc362bbc17dde872b223be8554f2
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-17 01:04:40 +0300

    auto push

--- test/main.c
+++ test/main.c
@@ -11,31 +11,58 @@ int main(void)
 {
     int len;
     int len2;
-    int test;	
-    char *str = "fivee";
-	test=_printf("fivee");
-	printf("test five is %d\n",test);
-	test=_printf("%c",'k');	
- 	 printf("test one char is  %d\n",test); 
-     	
-	test=_printf("%s",str);
-         printf("test str five  is  %d\n",test);
-	
-	
-	
-	
-	 len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    printf("our is %d and original is %d",len,len2);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    len = _printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+    len2 = printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("Let's print a simple sentence.\n");
+    len2 = printf("Let's print a simple sentence.\n");
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("%c", 'S');
+    len2 = printf("%c", 'S');
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("A char inside a sentence: %c. Did it work?\n", 'F');
+    len2 = printf("A char inside a sentence: %c. Did it work?\n", 'F');
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("%s", "This sentence is retrieved from va_args!\n");
+    len2 = printf("%s", "This sentence is retrieved from va_args!\n");
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+    len2 = printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+    len2 = printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+    len2 = printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("%%");
+    len2 = printf("%%");
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("Should print a single percent sign: %%\n");
+    len2 = printf("Should print a single percent sign: %%\n");
+    printf("my code = %d and the original code = %d\n",len,len2);
+    len = _printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+    len2 = printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+    printf("my code = %d and the original code = %d\n",len,len2);
+    
+    
+
+
+
     return (0);
 }
