92e98adfc112bb525b2bb12fd2de1ba13e6a94ea
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-17 23:53:37 +0300

    auto push

--- test/nasser/main.h
+++ test/nasser/main.h
@@ -10,6 +10,7 @@ int _putchar(char);
 int _printf(const char *format, ...);
 int print_char(char arg, int count);
 int print_str(char *str, int c);
+int print_int(long int arg, int count);
 
 
 #endif
