ad2829e309d56f471b56a41d727a270c639a38c8
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-18 16:20:59 +0300

    auto push

--- dec_to_bin.c
+++ dec_to_bin.c
@@ -1,16 +1,24 @@
 #include "main.h"
-int dec_to_bin(unsigned int u,int c);
-unsigned int get_power(int num, int power);
 
-int dec_to_bin(unsigned int u,int c)
+/**
+ *dec_to_bin- is a converter function to binary.
+ *
+ *@u :is the dicemal muber.
+ *@c :is the counter for printed chars.
+ *
+ *Return: always (counter)
+ */
+
+int dec_to_bin(unsigned int u, int c)
 {
 	int i;
-	char b[] = "00000000";
-	for (i = 7; i >= 0; --i)
-		if (u >= get_power(2,i))
+	char b[] = "00000000000000000000000000000000";
+
+	for (i = 31; i >= 0; --i)
+		if (u >= get_power(2, i))
 		{
-			b[7 - i] = '1';
-			u -= get_power(2,i);
+			b[31 - i] = '1';
+			u -= get_power(2, i);
 		}
 	for (i = 0; b[i] != '\0'; ++i)
 	{
@@ -19,9 +27,18 @@ int dec_to_bin(unsigned int u,int c)
 	}
 	return (c);
 }
+
+/**
+ *get_power- is a function to get power.
+ *
+ *@num : is the number to get itis power.
+ *@power : is the power.
+ *
+ *Return: always (power)
+ */
 unsigned int get_power(int num, int power)
 {
 	if (power == 0)
 		return (1);
-	return(num * get_power(num ,power- 1) );
+	return (num * get_power(num, power - 1));
 }
--- main.h
+++ main.h
@@ -11,7 +11,7 @@ int _printf(const char *format, ...);
 int print_char(char arg, int count);
 int print_str(char *str, int c);
 int print_int(long int arg, int count);
-int dec_to_bin(unsigned int u,int c);
+int dec_to_bin(unsigned int u, int c);
 unsigned int get_power(int num, int power);
 
 
