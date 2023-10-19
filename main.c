1e3f1e1ab625b6621e2d2e24cfe49e888788477b
kedwani linux <mahmoudnasser11566@gmail.com>
2023-10-19 18:12:19 +0300

    auto push

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main()
-{
-	int i = _printf("%S", "No special character.");
-	printf("\n%d\n",i);
-
-	return (0);
-}
--- main.h
+++ main.h
@@ -16,6 +16,7 @@ int print_str(char *str, int c);
 int print_int(long int arg, int count, char flag);
 int dec_to_bin(unsigned int u, int c);
 int dec_to_hex(int decimalNumber, int c);
+int print_Str(char *str, int c);
 
 
 #endif
